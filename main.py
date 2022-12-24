f41abd623d67843cbc818179cd546e04894991a2
ykdman <dbsrudejr300@gmail.com>
2022-12-24 18:25:02 +0900

    First Create

---
+++ main.py
@@ -0,0 +1,53 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    print(count)
+    if count > 0 :
+        window.after(1000, count_down, count - 1)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50))
+title_label.config(bg=YELLOW)
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)  # highlightthickness=0 경계선 없애기
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+canvas.grid(column=1, row=1)
+
+count_down(5)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_mark.grid(column=1, row=3)
+
+
+window.mainloop()
